1396f6eba37b40c1c5c354e31ab52afc74643339
Arty <arty@arty.com>
2024-12-07 19:10:40 -0500

    convert mask to simple enum

--- Token.cs
+++ Token.cs
@@ -3,10 +3,10 @@
     public enum TokenType
     {
         None,
-        Text = 1 << 0,
-        Whitespace = 1 << 2,
-        Control = 1 << 3,
-        CachedData = 1 << 4
+        Text,
+        Whitespace,
+        Control,
+        CachedData
     }
 
     public class Token
